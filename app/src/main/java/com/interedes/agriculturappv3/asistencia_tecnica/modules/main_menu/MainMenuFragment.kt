bd80599271837551bde2becef09195363df9f346
SebastianPolania <sebastianpolaniadiaz@gmail.com>
2018-03-07 14:19:17 -0500

    Merge branch 'master' of /home/Tatan/AndroidStudioProjects/AgriculturaApp/AgriculturappAndroid with conflicts.

--- app/src/main/java/com/interedes/agriculturappv3/asistencia_tecnica/modules/main_menu/MainMenuFragment.kt
+++ app/src/main/java/com/interedes/agriculturappv3/asistencia_tecnica/modules/main_menu/MainMenuFragment.kt
@@ -1,6 +1,7 @@
 package com.interedes.agriculturappv3.asistencia_tecnica.modules.main_menu
 
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.GridLayoutManager
@@ -9,6 +10,7 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.interedes.agriculturappv3.R
+import com.interedes.agriculturappv3.asistencia_tecnica.activities.MapsActivity
 import com.interedes.agriculturappv3.asistencia_tecnica.adapters.SingleAdapter
 import com.interedes.agriculturappv3.asistencia_tecnica.modules.accounting_module.AccountingFragment
 import com.interedes.agriculturappv3.asistencia_tecnica.modules.asistencia_tecnica_module.AsistenciaTecnicaFragment
