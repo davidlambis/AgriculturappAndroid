02268f2d7eeaf46cdb946a309fefa7024ca5e333
davidlambis <davidlambis261@gmail.com>
2018-04-10 17:39:54 -0500

    Ofertas-comercial

--- app/src/main/java/com/interedes/agriculturappv3/productor/modules/comercial_module/ofertas/OfertasFragment.kt
+++ app/src/main/java/com/interedes/agriculturappv3/productor/modules/comercial_module/ofertas/OfertasFragment.kt
@@ -1,20 +1,53 @@
 package com.interedes.agriculturappv3.productor.modules.comercial_module.ofertas
 
 
+import android.content.DialogInterface
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.widget.SwipeRefreshLayout
+import android.support.v7.app.AlertDialog
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import com.afollestad.materialdialogs.DialogAction
+import com.afollestad.materialdialogs.GravityEnum
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.Theme
 
 import com.interedes.agriculturappv3.R
+import com.interedes.agriculturappv3.productor.models.Cultivo
+import com.interedes.agriculturappv3.productor.models.Lote
+import com.interedes.agriculturappv3.productor.models.UnidadProductiva
+import com.interedes.agriculturappv3.productor.models.ofertas.Oferta
+import com.interedes.agriculturappv3.productor.models.producto.Producto
+import com.interedes.agriculturappv3.productor.modules.comercial_module.ofertas.adapters.OfertasAdapter
+import com.interedes.agriculturappv3.productor.modules.ui.main_menu.MenuMainActivity
+import kotlinx.android.synthetic.main.activity_menu_main.*
+import kotlinx.android.synthetic.main.content_recyclerview.*
+import kotlinx.android.synthetic.main.dialog_select_spinners.view.*
+import kotlinx.android.synthetic.main.fragment_ofertas.*
+import java.util.ArrayList
 
 
-/**
- * A simple [Fragment] subclass.
- */
-class OfertasFragment : Fragment() {
+class OfertasFragment : Fragment(), IOfertas.View, SwipeRefreshLayout.OnRefreshListener, View.OnClickListener {
 
+    var presenter: IOfertas.Presenter? = null
+    var ofertasList: ArrayList<Oferta>? = ArrayList<Oferta>()
+    var adapter: OfertasAdapter? = null
+    var viewDialogFilter: View? = null
+    var cultivoGlobal: Cultivo? = null
+    var unidadProductivaGlobal: UnidadProductiva? = null
+    var loteGlobal: Lote? = null
+    var productoGlobal: Producto? = null
+    var _dialogFilter: MaterialDialog? = null
+
+    var Producto_Id: Long? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -22,4 +55,258 @@ class OfertasFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_ofertas, container, false)
     }
 
-}// Required empty public constructor
+    companion object {
+        var instance: OfertasFragment? = null
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        OfertasFragment.instance = this
+        presenter = OfertasPresenter(this)
+        presenter?.onCreate()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initAdapter()
+        (activity as MenuMainActivity).toolbar.title = getString(R.string.title_ofertas)
+        ivBackButton.setOnClickListener(this)
+        swipeRefreshLayout.setOnRefreshListener(this)
+        searchFilter.setOnClickListener(this)
+        setupInjection()
+    }
+
+    private fun initAdapter() {
+        recyclerView?.layoutManager = LinearLayoutManager(activity)
+        adapter = OfertasAdapter(ofertasList!!)
+        recyclerView?.adapter = adapter
+    }
+
+    private fun setupInjection() {
+        presenter?.getListOfertas(Producto_Id)
+    }
+
+    //region on Click
+    override fun onClick(p0: View?) {
+        when (p0?.id) {
+            R.id.searchFilter -> {
+                showAlertDialogFilterOferta()
+            }
+        }
+    }
+    //endregion
+
+    //region Métodos Interfaz
+    override fun showProgress() {
+        swipeRefreshLayout.isRefreshing = true
+    }
+
+    override fun hideProgress() {
+        swipeRefreshLayout.isRefreshing = false
+    }
+
+    override fun setListOfertas(listOfertas: List<Oferta>) {
+        adapter?.clear()
+        ofertasList?.clear()
+        adapter?.setItems(listOfertas)
+        hideProgress()
+        setResults(listOfertas.size)
+    }
+
+    override fun setResults(ofertas: Int) {
+        var results = String.format(getString(R.string.results_global_search),
+                ofertas)
+        txtResults.setText(results)
+    }
+
+    override fun validarListas(): Boolean? {
+        var cancel = false
+        var focusView: View? = null
+        if (viewDialogFilter?.spinnerUnidadProductiva?.text.toString().isEmpty()) {
+            viewDialogFilter?.spinnerUnidadProductiva?.setError(getString(R.string.error_field_required))
+            focusView = viewDialogFilter?.spinnerUnidadProductiva
+            cancel = true
+        } else if (viewDialogFilter?.spinnerLote?.text.toString().isEmpty()) {
+            viewDialogFilter?.spinnerLote?.setError(getString(R.string.error_field_required))
+            focusView = viewDialogFilter?.spinnerLote
+            cancel = true
+        } else if (viewDialogFilter?.spinnerCultivo?.text.toString().isEmpty()) {
+            viewDialogFilter?.spinnerCultivo?.setError(getString(R.string.error_field_required))
+            focusView = viewDialogFilter?.spinnerCultivo
+            cancel = true
+        } else if (viewDialogFilter?.spinnerProducto?.text.toString().isEmpty()) {
+            viewDialogFilter?.spinnerProducto?.setError(getString(R.string.error_field_required))
+            focusView = viewDialogFilter?.spinnerProducto
+            cancel = true
+        }
+        if (cancel) {
+            focusView?.requestFocus()
+        } else {
+            return true;
+        }
+        return false
+    }
+
+    override fun showAlertDialogFilterOferta() {
+        val inflater = this.layoutInflater
+        viewDialogFilter = inflater.inflate(R.layout.dialog_select_spinners, null)
+        viewDialogFilter?.spinnerProducto?.visibility = View.VISIBLE
+        presenter?.setListSpinnerUnidadProductiva()
+
+        val title: String? = getString(R.string.select_product_ofertas)
+        if (unidadProductivaGlobal != null && loteGlobal != null && cultivoGlobal != null && productoGlobal != null) {
+            presenter?.setListSpinnerLote(unidadProductivaGlobal?.Id)
+            presenter?.setListSpinnerCultivo(loteGlobal?.Id)
+            presenter?.setListSpinnerProducto(cultivoGlobal?.Id)
+
+            viewDialogFilter?.spinnerUnidadProductiva?.setText(unidadProductivaGlobal?.Nombre)
+            viewDialogFilter?.spinnerLote?.setText(loteGlobal?.Nombre)
+            viewDialogFilter?.spinnerCultivo?.setText(cultivoGlobal?.Nombre)
+            viewDialogFilter?.spinnerProducto?.setText(productoGlobal?.Nombre)
+        }
+
+        viewDialogFilter?.ivCloseButtonDialogFilter?.setOnClickListener(this)
+
+        val dialog = MaterialDialog.Builder(activity!!)
+                .title(title!!)
+                .customView(viewDialogFilter!!, true)
+                .positiveText(R.string.confirm)
+                .negativeText(R.string.close)
+                .titleGravity(GravityEnum.CENTER)
+                .titleColorRes(R.color.light_green_800)
+                .limitIconToDefaultSize()
+                //.maxIconSizeRes(R.dimen.text_size_40)
+                // .positiveColorRes(R.color.material_red_400)
+                .backgroundColorRes(R.color.white_solid)
+                // .negativeColorRes(R.color.material_red_400)
+                .iconRes(R.drawable.ic_lote)
+                .dividerColorRes(R.color.colorPrimary)
+                .contentColorRes(android.R.color.white)
+                .btnSelector(R.drawable.md_btn_selector_custom, DialogAction.POSITIVE)
+                .positiveColor(Color.WHITE)
+                .autoDismiss(false)
+                //.negativeColorAttr(android.R.attr.textColorSecondaryInverse)
+                .theme(Theme.DARK)
+                .onPositive(
+                        { dialog1, which ->
+                            if (presenter?.validarListas() == true) {
+                                dialog1.dismiss()
+                                presenter?.getListOfertas(Producto_Id)
+                                presenter?.getProducto(Producto_Id)
+                            }
+                        })
+                .onNegative({ dialog1, which ->
+                    dialog1.dismiss()
+                })
+                .build()
+
+
+        val lp = WindowManager.LayoutParams()
+        lp.copyFrom(dialog.getWindow().getAttributes())
+        lp.width = WindowManager.LayoutParams.MATCH_PARENT
+        lp.height = WindowManager.LayoutParams.MATCH_PARENT
+        dialog.show()
+        dialog.getWindow().setAttributes(lp)
+//        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        _dialogFilter = dialog
+
+    }
+
+    override fun setListUnidadProductiva(listUnidadProductiva: List<UnidadProductiva>?) {
+        if (viewDialogFilter != null) {
+            viewDialogFilter?.spinnerUnidadProductiva!!.setAdapter(null)
+            val unidadProductivaArrayAdapter = ArrayAdapter<UnidadProductiva>(activity, android.R.layout.simple_spinner_dropdown_item, listUnidadProductiva)
+            viewDialogFilter?.spinnerUnidadProductiva!!.setAdapter(unidadProductivaArrayAdapter)
+            viewDialogFilter?.spinnerUnidadProductiva!!.onItemClickListener = AdapterView.OnItemClickListener { adapterView, view, position, l ->
+                viewDialogFilter?.spinnerLote?.setText("")
+                viewDialogFilter?.spinnerLote?.setHint(String.format(getString(R.string.spinner_lote)))
+                viewDialogFilter?.spinnerCultivo?.setText("")
+                viewDialogFilter?.spinnerCultivo?.setHint(String.format(getString(R.string.spinner_cultivo)))
+                unidadProductivaGlobal = listUnidadProductiva!![position] as UnidadProductiva
+                presenter?.setListSpinnerLote(unidadProductivaGlobal?.Id)
+            }
+            presenter?.setListSpinnerLote(null)
+            presenter?.setListSpinnerCultivo(null)
+        }
+    }
+
+    override fun setListLotes(listLotes: List<Lote>?) {
+        viewDialogFilter?.spinnerLote!!.setAdapter(null)
+        //viewDialogFilter?.spinnerLote?.setText("")
+        //viewDialogFilter?.spinnerLote?.setHint(String.format(getString(R.string.spinner_lote)))
+        val loteArrayAdapter = ArrayAdapter<Lote>(activity, android.R.layout.simple_spinner_dropdown_item, listLotes)
+        viewDialogFilter?.spinnerLote!!.setAdapter(loteArrayAdapter)
+        viewDialogFilter?.spinnerLote!!.onItemClickListener = AdapterView.OnItemClickListener { adapterView, view, position, l ->
+            viewDialogFilter?.spinnerCultivo?.setText("")
+            viewDialogFilter?.spinnerCultivo?.setHint(String.format(getString(R.string.spinner_cultivo)))
+            loteGlobal = listLotes!![position]
+            presenter?.setListSpinnerCultivo(loteGlobal?.Id)
+        }
+    }
+
+    override fun setListCultivos(listCultivos: List<Cultivo>?) {
+        viewDialogFilter?.spinnerCultivo!!.setAdapter(null)
+        //viewDialogFilter?.spinnerCultivo?.setText("")
+        //viewDialogFilter?.spinnerCultivo?.setHint(String.format(getString(R.string.spinner_cultivo)))
+        val cultivoArrayAdapter = ArrayAdapter<Cultivo>(activity, android.R.layout.simple_spinner_dropdown_item, listCultivos)
+        viewDialogFilter?.spinnerCultivo!!.setAdapter(cultivoArrayAdapter)
+        viewDialogFilter?.spinnerCultivo!!.onItemClickListener = AdapterView.OnItemClickListener { adapterView, view, position, l ->
+            cultivoGlobal = listCultivos!![position]
+            presenter?.setListSpinnerProducto(cultivoGlobal?.Id)
+        }
+    }
+
+    override fun setListProductos(listProductos: List<Producto>?) {
+        viewDialogFilter?.spinnerProducto!!.setAdapter(null)
+        //viewDialogFilter?.spinnerCultivo?.setText("")
+        //viewDialogFilter?.spinnerCultivo?.setHint(String.format(getString(R.string.spinner_cultivo)))
+        val productoArrayAdapter = ArrayAdapter<Producto>(activity, android.R.layout.simple_spinner_dropdown_item, listProductos)
+        viewDialogFilter?.spinnerProducto!!.setAdapter(productoArrayAdapter)
+        viewDialogFilter?.spinnerProducto!!.onItemClickListener = AdapterView.OnItemClickListener { adapterView, view, position, l ->
+            productoGlobal = listProductos!![position]
+            Producto_Id = productoGlobal?.Id
+        }
+    }
+
+    override fun verificateConnection(): AlertDialog? {
+        val builder = AlertDialog.Builder(context!!)
+        builder.setTitle(getString(R.string.alert))
+        builder.setMessage(getString(R.string.verificate_conexion))
+        builder.setPositiveButton(getString(R.string.confirm), DialogInterface.OnClickListener { dialog, which ->
+            dialog.dismiss()
+        })
+        builder.setIcon(R.drawable.ic_ofertas)
+        return builder.show()
+    }
+
+    override fun onEventBroadcastReceiver(extras: Bundle, intent: Intent) {
+        if (extras != null) {
+            if (extras.containsKey("state_conectivity")) {
+                var state_conectivity = intent.extras!!.getBoolean("state_conectivity")
+            }
+        }
+    }
+
+    override fun confirmDelete(oferta: Oferta): AlertDialog? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun setProducto(producto: Producto?) {
+        if (productoSelectedContainer.visibility == View.GONE) {
+            productoSelectedContainer.visibility = View.VISIBLE
+        }
+        txtNombreCultivo?.setText(producto?.NombreCultivo)
+        txtNombreDetalleTipoProducto?.setText(producto?.NombreDetalleTipoProducto)
+        txtPrecioProducto?.setText(producto?.Precio.toString())
+        txtFechaDisponibilidadProducto?.setText(producto?.getFechaLimiteDisponibilidadFormat())
+    }
+    //endregion
+
+    //region Methods
+    override fun onRefresh() {
+        showProgress()
+        presenter?.getListOfertas(Producto_Id)
+    }
+    //endregion
+
+}
--- app/src/main/java/com/interedes/agriculturappv3/productor/modules/comercial_module/ofertas/adapters/OfertasAdapter.kt
+++ app/src/main/java/com/interedes/agriculturappv3/productor/modules/comercial_module/ofertas/adapters/OfertasAdapter.kt
@@ -1,4 +1,62 @@
 package com.interedes.agriculturappv3.productor.modules.comercial_module.ofertas.adapters
 
-class OfertasAdapter {
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.interedes.agriculturappv3.R
+import com.interedes.agriculturappv3.libs.EventBus
+import com.interedes.agriculturappv3.libs.GreenRobotEventBus
+import com.interedes.agriculturappv3.productor.models.ofertas.Oferta
+import com.interedes.agriculturappv3.productor.modules.comercial_module.ofertas.events.OfertasEvent
+import java.util.ArrayList
+
+class OfertasAdapter(var lista: ArrayList<Oferta>) : RecyclerView.Adapter<OfertasAdapter.ViewHolder>() {
+
+    init {
+        eventBus = GreenRobotEventBus()
+    }
+
+    companion object {
+        var eventBus: EventBus? = null
+        fun postEvent(type: Int, oferta: Oferta?) {
+            val ofertaMutable = oferta as Object
+            val event = OfertasEvent(type, null, ofertaMutable, null)
+            event.eventType = type
+            eventBus?.post(event)
+        }
+    }
+
+    override fun onBindViewHolder(holder: OfertasAdapter.ViewHolder, position: Int) {
+        //Llama al método del holder para cargar los items
+        holder.bindItems(lista[position], position)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OfertasAdapter.ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.content_list_general, parent, false)
+        return OfertasAdapter.ViewHolder(v)
+    }
+
+    override fun getItemCount(): Int {
+        return lista.size
+    }
+
+
+    fun setItems(newItems: List<Oferta>) {
+        lista.addAll(newItems)
+        notifyDataSetChanged()
+
+    }
+
+    fun clear() {
+        lista.clear()
+        notifyDataSetChanged()
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bindItems(data: Oferta, pos: Int) = with(itemView) {
+
+
+        }
+    }
 }
