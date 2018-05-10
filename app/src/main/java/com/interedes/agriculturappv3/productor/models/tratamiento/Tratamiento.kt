b16104d44e87517edb4ccd10e70ad43364aba087
enu-44 <enuarmunozcastillo@gmail.com>
2018-05-09 19:20:27 -0500

    Add services remote tipo productos, enfermedades, tratamientos, insumos, changes lista de plagas

---
+++ app/src/main/java/com/interedes/agriculturappv3/productor/models/tratamiento/Tratamiento.kt
@@ -0,0 +1,65 @@
+package com.interedes.agriculturappv3.productor.models.tratamiento
+
+import com.google.gson.annotations.SerializedName
+import com.interedes.agriculturappv3.config.DataSource
+import com.interedes.agriculturappv3.productor.models.insumos.Insumo
+import com.raizlabs.android.dbflow.annotation.Column
+import com.raizlabs.android.dbflow.annotation.PrimaryKey
+import com.raizlabs.android.dbflow.annotation.Table
+
+@Table(database = DataSource::class)
+data class Tratamiento(@PrimaryKey
+                       @SerializedName("Id")
+                       @Column(name = "Id")
+                       var Id: Long? = 0,
+
+                       @SerializedName("DetalleCategoria_InsumoId")
+                       @Column(name = "DetalleCategoria_InsumoId")
+                       var DetalleCategoria_InsumoId: Long? = 0,
+
+                       @SerializedName("Desc_Aplicacion")
+                       @Column(name = "Desc_Aplicacion")
+                       var Desc_Aplicacion: String? = null,
+
+                       @SerializedName("Desc_Formulacion")
+                       @Column(name = "Desc_Formulacion")
+                       var Desc_Formulacion: String? = null,
+
+                       @SerializedName("IngredienteActivo")
+                       @Column(name = "IngredienteActivo")
+                       var IngredienteActivo: String? = null,
+
+                       @SerializedName("InsumoId")
+                       @Column(name = "InsumoId")
+                       var InsumoId: Long? = 0,
+
+                       @SerializedName("Modo_Accion")
+                       @Column(name = "Modo_Accion")
+                       var Modo_Accion: String? = null,
+
+                       @SerializedName("Nombre_Comercial")
+                       @Column(name = "Nombre_Comercial")
+                       var Nombre_Comercial: String? = null,
+
+                       @SerializedName("precioAproximado")
+                       @Column(name = "precioAproximado")
+                       var precioAproximado: Double? = 0.0,
+
+                       @SerializedName("proveedor")
+                       @Column(name = "proveedor")
+                       var proveedor: String? = null,
+
+                       @SerializedName("EnfermedadesId")
+                       @Column(name = "EnfermedadesId")
+                       var EnfermedadesId: Long? = 0,
+
+
+                       @SerializedName("Insumo")
+                       var Insumo: Insumo?=Insumo(),
+
+                       @SerializedName("Calificacions")
+                       var Calificacions: ArrayList<Calificacion_Tratamiento>?= ArrayList<Calificacion_Tratamiento>()
+
+
+) {
+}
